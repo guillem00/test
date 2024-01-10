1d88023c75864d772061784ca1c130a615e1bf08
Guillem <a2208057@institutmontilivi.cat>
2024-01-10 12:08:31 +0100

    Fitxers de contadors php

---
+++ contador.php
@@ -0,0 +1,12 @@
+<?php
+$count=$ COOKIE['count'];
+
+if(isset($count)){
+$count++;
+}else{
+$count=0;
+}
+
+setcookie('count',$count);
+echo $count;
+?>
---
+++ contador4.php
@@ -0,0 +1,31 @@
+<?php
+
+ini_set('display_errors',1);
+ini_set('display_stsrtup_errors',1);
+error_reporting(E_ALL);
+
+$servername = "localhost";
+$username = "admin";
+$password = "12345";
+$dbname = "estadistics";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if($conn -> connect_errno){
+
+echo "Error en connectar a Mysql: " . $conn -> connect_error;
+exit();
+}
+
+$ip=$_SERVER['REMOTE_ADDR'];
+$sql="INSERT INTO registre(ip) VALUES ('" . $ip . "')";
+$conn->query($sql);
+
+$resultat = $conn->query("SELECT COUNT(*) FROM registre");
+$row = mysqli_fetch_array($resultat);
+echo $row[0];
+
+$conn->close();
+
+
+?>
